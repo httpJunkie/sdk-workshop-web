89799cb85ca15129f655a8075d44dd34eca49f72
httpjunkie <eric@httpjunkie.com>
2023-11-03 13:08:55 -0400

    update App.tsx to include env var for Infura Key deleted prior key that was uploaded

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const { chains } = configureChains(
   [
     jsonRpcProvider({
       rpc: () => ({
-        http: `https://linea-goerli.infura.io/v3/305727869d444a8f8e17345b4d8b32e7`,
+        http: `https://linea-goerli.infura.io/v3/${import.meta.env.VITE_PUBLIC_INFURA_API_KEY}`,
       }),
     }),
   ],
