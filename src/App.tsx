151250a4065792e55c0f8ea93277e9c8e3a9dcbf
httpjunkie <eric@httpjunkie.com>
2023-11-03 12:07:22 -0400

    Remove unneeded WAGMI configuration

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,11 @@ import { Display } from './components/Display'
 import { MetaMaskError } from './components/MetaMaskError'
 import { AppContextProvider } from './hooks/useAppContext'
 
-import { WagmiConfig, createConfig, configureChains } from 'wagmi'
+import { configureChains } from 'wagmi'
 import { lineaTestnet } from '@wagmi/core/chains'
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
-import { InjectedConnector } from 'wagmi/connectors/injected'
 
-const { chains, publicClient } = configureChains(
+const { chains } = configureChains(
   [lineaTestnet],
   [
     jsonRpcProvider({
@@ -22,20 +21,6 @@ const { chains, publicClient } = configureChains(
     })]
   ,
 )
-
-const config = createConfig({
-  autoConnect: true,
-  publicClient,
-  connectors: [
-    new InjectedConnector({
-      chains,
-      options: {
-        name: 'injected',
-        shimDisconnect: true,
-      },
-    }),
-  ],
-})
 
 export const App = () => {
   const sdkOptions = {
@@ -48,16 +33,14 @@ export const App = () => {
   }
 
   return (
-    <WagmiConfig config={config}>
-      <AppContextProvider>
-        <MetaMaskUIProvider sdkOptions={sdkOptions} networks={chains}>
-          <div className={styles.appContainer}>
-            <Navigation />
-            <Display />
-            <MetaMaskError />
-          </div>
-        </MetaMaskUIProvider>
-      </AppContextProvider>
-    </WagmiConfig>
+    <AppContextProvider>
+      <MetaMaskUIProvider sdkOptions={sdkOptions} networks={chains}>
+        <div className={styles.appContainer}>
+          <Navigation />
+          <Display />
+          <MetaMaskError />
+        </div>
+      </MetaMaskUIProvider>
+    </AppContextProvider>
   )
 }
